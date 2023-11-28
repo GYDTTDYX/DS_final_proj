515bd534938ccb3c984ca735b31f94ed818f84cb
chenshuopan6 <cp3384@cumc.columbia.edu>
2023-11-27 22:54:55 -0500

    Added some pictures, rewritten js files, added footer and some style changes

--- script.js
+++ script.js
@@ -1,32 +1,18 @@
-document.addEventListener('DOMContentLoaded', function () {
+function toggleContent(contentType) {
     var videoButton = document.getElementById('videoButton');
     var descriptionButton = document.getElementById('descriptionButton');
     var videoContent = document.getElementById('videoContent');
-    var textContent = document.getElementById('textContent');
     var descriptionContent = document.getElementById('descriptionContent');
 
-
-    videoButton.classList.add('active');
-
-    videoButton.addEventListener('click', function () {
-
+    if (contentType === 'video') {
         videoButton.classList.add('active');
         descriptionButton.classList.remove('active');
-
-
         videoContent.classList.add('active');
-        textContent.classList.remove('active');
         descriptionContent.classList.remove('active');
-    });
-
-    descriptionButton.addEventListener('click', function () {
-
+    } else if (contentType === 'description') {
         videoButton.classList.remove('active');
         descriptionButton.classList.add('active');
-
-
         videoContent.classList.remove('active');
-        textContent.classList.remove('active');
         descriptionContent.classList.add('active');
-    });
-});
+    }
+}
